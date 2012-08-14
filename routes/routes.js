fe1eff7cdc75601b1535149a1d5a23ecbfdffbaf
Ted Conn <citizen@tedconn.com>
2012-08-13 21:14:15 -0700

    added routes file

---
+++ routes/routes.js
@@ -0,0 +1,8 @@
+/**
+ * User: citizen
+ * Date: 8/13/12
+ * Time: 9:09 PM
+ */
+exports.index = function(req, res){
+    res.send("hi p37");
+}
--- web.js
+++ web.js
@@ -1,10 +1,8 @@
 var express = require('express');
+var routes = require('./routes');
+var app = express();
 
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response){
-  response.send('Hello World');
-});
+app.get('/', routes.index);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function(){
